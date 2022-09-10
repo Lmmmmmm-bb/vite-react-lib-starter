ac6e6a99512238cfa512c36c1c43d4dfaa82b863
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-11 01:07:11 +0800

    fix: gh pages base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-react-lib-starter/',
   plugins: [Dts({ staticImport: true, insertTypesEntry: true }), react()],
   resolve: {
     alias: {
