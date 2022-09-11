0468542072311619614fb5a65aeb8935c61ba2fc
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-12 01:35:15 +0800

    chore: clean up

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-react-lib-starter/',
   plugins: [react()],
   resolve: {
     alias: {
