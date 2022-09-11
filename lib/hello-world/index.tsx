4053715bcaacd8e8da1b5224c3cb4c03cc33da12
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-11 10:59:58 +0800

    feat: styled component support

--- lib/hello-world/index.tsx
+++ lib/hello-world/index.tsx
@@ -1,5 +1,12 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-const HelloWorld: FC = () => <div>Hello World Component</div>;
+const Wrapper = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const HelloWorld: FC = () => <Wrapper>Hello World Component</Wrapper>;
 
 export default HelloWorld;
