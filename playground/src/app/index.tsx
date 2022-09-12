b374786a0fed5715f9ef205d968069687229fa7d
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-12 18:33:04 +0800

    chore: playground clean up

--- playground/src/app/index.tsx
+++ playground/src/app/index.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 import { HelloWorld } from 'vite-react-lib-starter';
+
+const StyledWrapper = styled.div`
+  text-align: center;
+`;
 
 const App: FC = () => {
   return (
-    <div>
+    <StyledWrapper>
       Hello Playground
       <HelloWorld message='Vite React Lib Starter'>This is children</HelloWorld>
-    </div>
+    </StyledWrapper>
   );
 };
 
